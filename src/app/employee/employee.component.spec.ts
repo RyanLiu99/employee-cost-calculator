b99ba1fa277205beccdf1b1e4b64ce025bc5fca3
Ryan Liu <RyanLiu_99@hotmail.com>
2020-06-23 12:16:45 -0700

    fix unit tests

--- src/app/employee/employee.component.spec.ts
+++ src/app/employee/employee.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeeComponent } from './employee.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('EmployeeComponent', () => {
   let component: EmployeeComponent;
@@ -8,7 +10,8 @@ describe('EmployeeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeComponent ]
+      declarations: [ EmployeeComponent ],
+      imports: [AppRoutingModule, FormsModule, ReactiveFormsModule]
     })
     .compileComponents();
   }));
